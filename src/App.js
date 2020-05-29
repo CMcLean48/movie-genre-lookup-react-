8f1a2aa76e0d02b5803b7dd349b9198919b72453
crystal mclean <cmclean48@my.bcit.ca>
2020-05-28 21:49:18 -0700

    change to https

--- src/App.js
+++ src/App.js
@@ -1,187 +1,237 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Movielist from './Movielist';
 
-const API_KEY   = 'f8b5c4c831f66ef931d7c63686ffe0c8';
-const BASE_URL  = 'http://api.themoviedb.org/3/discover/movie?api_key='
-                + API_KEY;
+const API_KEY = 'f8b5c4c831f66ef931d7c63686ffe0c8';
+const BASE_URL =
+	'https://api.themoviedb.org/3/discover/movie?api_key=' + API_KEY;
 
-const GENRES = 'https://api.themoviedb.org/3/genre/movie/list?api_key='
-             + API_KEY
-             + '&language=en-US';
+const GENRES =
+	'https://api.themoviedb.org/3/genre/movie/list?api_key=' +
+	API_KEY +
+	'&language=en-US';
 
 class App extends Component {
-    
-    constructor() {
-        super();
-        
-        //get current date, plus a date 4 months in past,formatted for the api query.
-       // let d = new Date(),
-    let d = new Date(),
-		end = d.getFullYear() + '-' + `${d.getMonth() + 1}`.padStart(2, '0') + '-' + `${d.getDate()}`.padStart(2, '0');
+	constructor() {
+		super();
 
-         let e = d.setDate(d.getDate() - 120),
-				start = d.getFullYear() + '-' + `${d.getMonth() + 1}`.padStart(2, '0') + '-' + `${d.getDate()}`.padStart(2, '0');
-            //(d.getMonth() + 1)+'-'+ (d.getDate());
+		//get current date, plus a date 4 months in past,formatted for the api query.
+		// let d = new Date(),
+		let d = new Date(),
+			end =
+				d.getFullYear() +
+				'-' +
+				`${d.getMonth() + 1}`.padStart(2, '0') +
+				'-' +
+				`${d.getDate()}`.padStart(2, '0');
 
-           console.log(e);
+		let e = d.setDate(d.getDate() - 120),
+			start =
+				d.getFullYear() +
+				'-' +
+				`${d.getMonth() + 1}`.padStart(2, '0') +
+				'-' +
+				`${d.getDate()}`.padStart(2, '0');
+		//(d.getMonth() + 1)+'-'+ (d.getDate());
 
-        this.state  = {
-          apiKey : API_KEY,
-          start : start,
-          end    : end,
-          genre : "28 ",
-          page:1,
-          loading:'true',
-          movies : [],
-          genres : [],
-          selectedGenre  : '',
-          overview: false,
-          id : '',
-          title: '',
-          release_date:'',         
-        };
-        this.getMovies = this.getMovies.bind(this);
-        this.handleGenreChange = this.handleGenreChange.bind(this);
-        this.nextPage  = this.nextPage.bind(this);
-        this.previousPage = this.previousPage.bind(this);
-       
-    }
-    
-    // Called when constructor is finished building component.
-    componentDidMount() {
-      this.getMovies(this.state.page, this.state.start, this.state.end, this.state.genre);
-      this.getGenres();
-    }
+		console.log(e);
 
-    handleGenreChange(e) {
-      this.setState({ selectedGenre: e.target.value, page: 1 });
-     // const url = "http://moviedb.com?genre=" + genre;
-   //   alert("Getting movie with genre id: " + genre + " at "
-    //      + url  );
-    this.getMovies(this.state.page, this.state.start, this.state.end, e.target.value);
-    }
-    
+		this.state = {
+			apiKey: API_KEY,
+			start: start,
+			end: end,
+			genre: '28 ',
+			page: 1,
+			loading: 'true',
+			movies: [],
+			genres: [],
+			selectedGenre: '',
+			overview: false,
+			id: '',
+			title: '',
+			release_date: '',
+		};
+		this.getMovies = this.getMovies.bind(this);
+		this.handleGenreChange = this.handleGenreChange.bind(this);
+		this.nextPage = this.nextPage.bind(this);
+		this.previousPage = this.previousPage.bind(this);
+	}
 
-    getMovies(page,start,end,genre) {
-     
-        // Build string with base and start and end dates, page number and genre.
-        const URL        = BASE_URL 
-                         + '&primary_release_date.gte=' + start
-           // + '&primary_release_date.gte=2020-01-01'
-                        +  '&primary_release_date.lte=' + end 
-                         + '&page=' + page 
-                         + '&with_genres=' + genre;
+	// Called when constructor is finished building component.
+	componentDidMount() {
+		this.getMovies(
+			this.state.page,
+			this.state.start,
+			this.state.end,
+			this.state.genre
+		);
+		this.getGenres();
+	}
 
-        // Request and wait for data from remote server.
-        fetch(URL).then(response => response.json())
-            // Data retrieved so parse it.
-            .then((data) => {
-                this.setState({movies:data.results});
-              //  alert("Total pages= " + data.total_pages)
-              //  console.log(JSON.stringify(data.results));
-                
-            
-              //set how many total pages for this instance of movies//
-                this.setState({totalPages : data.total_pages});
-            })
-            // Data is not retieved.
-            .catch((error) => {
-                alert(error);
+	handleGenreChange(e) {
+		this.setState({ selectedGenre: e.target.value, page: 1 });
+		// const url = "http://moviedb.com?genre=" + genre;
+		//   alert("Getting movie with genre id: " + genre + " at "
+		//      + url  );
+		this.getMovies(
+			this.state.page,
+			this.state.start,
+			this.state.end,
+			e.target.value
+		);
+	}
 
-            });                          
-    }
+	getMovies(page, start, end, genre) {
+		// Build string with base and start and end dates, page number and genre.
+		const URL =
+			BASE_URL +
+			'&primary_release_date.gte=' +
+			start +
+			// + '&primary_release_date.gte=2020-01-01'
+			'&primary_release_date.lte=' +
+			end +
+			'&page=' +
+			page +
+			'&with_genres=' +
+			genre;
 
-    getGenres() {
-        
-        // This code gets data from the remote server.
-        fetch(GENRES).then(response => response.json())
+		// Request and wait for data from remote server.
+		fetch(URL)
+			.then((response) => response.json())
+			// Data retrieved so parse it.
+			.then((data) => {
+				this.setState({ movies: data.results });
+				//  alert("Total pages= " + data.total_pages)
+				//  console.log(JSON.stringify(data.results));
 
-        // Data is retrieved.
-        .then((data) => {
-            this.setState({genres:data.genres, loading:false});
-          //  console.log(JSON.stringify(data.genres));
-        })
-        // Data is not retrieved.
-        .catch((error) => {
-            alert(error);
-        });
-    }
-        //to page forward//
-        nextPage(e){
-            if(this.state.page < this.state.totalPages){
-                let newPage = this.state.page
-                newPage = newPage +=1;
-                this.setState({page:newPage});
-                this.getMovies(newPage, this.state.start, this.state.end,  this.state.genre);
+				//set how many total pages for this instance of movies//
+				this.setState({ totalPages: data.total_pages });
+			})
+			// Data is not retieved.
+			.catch((error) => {
+				alert(error);
+			});
+	}
 
-            }     
-    }
-        //previous page//
-        previousPage(e){
-        if(this.state.page > 1 ){
-            let newPage = this.state.page
-            newPage = newPage -=1;
-            this.setState({page:newPage});
-            this.getMovies(newPage, this.state.start, this.state.end,  this.state.genre);
+	getGenres() {
+		// This code gets data from the remote server.
+		fetch(GENRES)
+			.then((response) => response.json())
 
-            }     
-    } 
-        
-    render() {
-        return (  
-    <div className="container">
-            <div className = "titleItem">
-                <table> 
-                    <tbody>
-                         <tr>
-                            <td>
-                            <img width="60" src="movie_roll.svg" alt="movieroll"></img>
-                            </td>
-                            <td>
-                            <h1>Movie Database Sort By Genre</h1>
-                            </td>
-                        </tr>
-                    </tbody>       
-                </table>
-            </div>        
-            <div >
-               {/* Genres */}
-               <p className="genreChangetitle">Change Genre</p>
-              <select className="selection"type='text' value={this.state.selectedGenre} 
-                     onChange={this.handleGenreChange}>                  
-                    {this.state.genres.map((item, index)=>(
-                <option key={item.id} value={item.id}>{item.name}</option>
-                ))}
-              </select>
-              {/* {this.state.selectedGenre} */}
-              <br/>
-              
-              <div className ="page">
-              
-              <button  onClick={this.previousPage}>&lt;&nbsp; Previous Page</button>
-              <p>&nbsp;Page Number:&nbsp;{this.state.page}&nbsp;of &nbsp;{this.state.totalPages} Pages</p>
-              &nbsp;<button onClick={this.nextPage}> Next Page&nbsp;&gt;</button>
-              <a className="anchor"href= "#bottom" id="top">go to bottom</a>
+			// Data is retrieved.
+			.then((data) => {
+				this.setState({ genres: data.genres, loading: false });
+				//  console.log(JSON.stringify(data.genres));
+			})
+			// Data is not retrieved.
+			.catch((error) => {
+				alert(error);
+			});
+	}
+	//to page forward//
+	nextPage(e) {
+		if (this.state.page < this.state.totalPages) {
+			let newPage = this.state.page;
+			newPage = newPage += 1;
+			this.setState({ page: newPage });
+			this.getMovies(
+				newPage,
+				this.state.start,
+				this.state.end,
+				this.state.genre
+			);
+		}
+	}
+	//previous page//
+	previousPage(e) {
+		if (this.state.page > 1) {
+			let newPage = this.state.page;
+			newPage = newPage -= 1;
+			this.setState({ page: newPage });
+			this.getMovies(
+				newPage,
+				this.state.start,
+				this.state.end,
+				this.state.genre
+			);
+		}
+	}
 
-             </div>
-             
-        
-          <Movielist movies={this.state.movies}></Movielist>
+	render() {
+		return (
+			<div className="container">
+				<div className="titleItem">
+					<table>
+						<tbody>
+							<tr>
+								<td>
+									<img
+										width="60"
+										src="movie_roll.svg"
+										alt="movieroll"
+									/>
+								</td>
+								<td>
+									<h1>Movie Database Sort By Genre</h1>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div>
+					{/* Genres */}
+					<p className="genreChangetitle">Change Genre</p>
+					<select
+						className="selection"
+						type="text"
+						value={this.state.selectedGenre}
+						onChange={this.handleGenreChange}
+					>
+						{this.state.genres.map((item, index) => (
+							<option key={item.id} value={item.id}>
+								{item.name}
+							</option>
+						))}
+					</select>
+					{/* {this.state.selectedGenre} */}
+					<br />
 
-          <footer>
-           
-          <button  onClick={this.previousPage}>&lt;&nbsp; Previous Page</button>
-          <button onClick={this.nextPage}> Next Page&nbsp;&gt;</button>
-          <a className ="anchor"href ="#top" id="bottom">go to top</a>
-          <p>© 2018 Crystal McLean</p>
-          </footer>
+					<div className="page">
+						<button onClick={this.previousPage}>
+							&lt;&nbsp; Previous Page
+						</button>
+						<p>
+							&nbsp;Page Number:&nbsp;{this.state.page}&nbsp;of
+							&nbsp;{this.state.totalPages} Pages
+						</p>
+						&nbsp;
+						<button onClick={this.nextPage}>
+							{' '}
+							Next Page&nbsp;&gt;
+						</button>
+						<a className="anchor" href="#bottom" id="top">
+							go to bottom
+						</a>
+					</div>
 
-     </div>  
-            
-                
-        </div>  
-        );
-    }
+					<Movielist movies={this.state.movies} />
+
+					<footer>
+						<button onClick={this.previousPage}>
+							&lt;&nbsp; Previous Page
+						</button>
+						<button onClick={this.nextPage}>
+							{' '}
+							Next Page&nbsp;&gt;
+						</button>
+						<a className="anchor" href="#top" id="bottom">
+							go to top
+						</a>
+						<p>© 2018 Crystal McLean</p>
+					</footer>
+				</div>
+			</div>
+		);
+	}
 }
 export default App;
